21a24808e12f35454ff2e6ecb48f0034f0be1071
Alex Cerezo <alexcerezocontreras@gmail.com>
2025-11-29 10:02:39 +0000

    Adding methods to the main in order to obtain the results

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from train import KNN, SVM, RF, NB, train_model
 from eval import get_metrics, get_roc_curve, evaluate_model
+from results import create_f1_comparison_table, create_summary_table
 
 methods = {
     "K" : KNN,
@@ -23,3 +24,9 @@ for method in methods.keys():
             for value in PCA:
                 y_test, y_pred, y_prob, model_name = train_model(method, iteration, transform, value)
                 evaluate_model(y_test, y_pred, y_prob, model_name)
+
+method_names = ['KNN', 'SVM', 'RF', 'NB']
+for method_name in method_names:
+    create_summary_table(method_name)          
+
+create_f1_comparison_table()                
